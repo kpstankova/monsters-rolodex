c541e9d46bbc9e417174e846e1b65a399ea53f90
kpstankova <krasimira579@gmail.com>
2020-10-01 18:05:36 +0300

    Updates

---
+++ precache-manifest.f6fceb69f9a4e6f1059235e07ed8de4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8e9d695289a49a7b4e3c8e724d4a23",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e61eea674d5aa904356b",
+    "url": "/monsters-rolodex/static/css/main.b3b2e017.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e61eea674d5aa904356b",
+    "url": "/monsters-rolodex/static/js/main.d070800f.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
